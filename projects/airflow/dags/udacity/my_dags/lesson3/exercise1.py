c63bec4d86bf12f8a0873fefa13a7ca12d5bad0d
adrien19 <n.adrien@mun.ca>
2019-08-05 02:23:16 -0230

    chore: updates plugin path on docker container

--- projects/airflow/dags/udacity/my_dags/lesson3/exercise1.py
+++ projects/airflow/dags/udacity/my_dags/lesson3/exercise1.py
@@ -13,7 +13,7 @@ from airflow import DAG
 from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 
-from airflow.operators.udacity_plugin import (
+from airflow.operators import (
     HasRowsOperator,
     PostgresOperator,
     PythonOperator,
